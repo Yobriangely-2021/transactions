c0a459803ec2d48be6b6d8894812e0c2fc89ec38
Yobriangely Mayora <yobriangely.mayora@gmail.com>
2021-05-06 20:28:15 -0300

    Comenzando el proceso de mi nuevo proyecto

---
+++ International.py
@@ -0,0 +1,41 @@
+print("País de transferencia")
+onename = input()
+print("Tipo de cuenta")
+holidays = input()
+print("Usuario")
+nameone = input()
+
+print("Información de la cuenta")
+print("Venezolana Yobriangely Mayora: Cliente número:106")
+SALDO = "40000 $ "
+numbercuenta = "0021458952"
+print(f"Número de cuenta {numbercuenta}")
+print(f"Saldo disponible {SALDO}")
+
+print("Nombre y número de cuenta para transferir ")
+Beatriz = input()
+
+print("Información de la cuenta")
+print("Venezolana Adriana Caraballo: Cliente número. 45")
+print("Número de cuenta: 60987545120")
+
+print("Informar el saldo de su tarjeta")
+dato1 = input()
+print(" Cantidad de dinero para transferir")
+dato2 = input()
+print("¿Acepta usted transferir este dinero?")
+yes = input()
+
+print("Dinero transferido correctamente")
+
+print("¿Quiere saber el saldo de su cuenta?")
+horno = input()
+
+numero1 = int(dato1)
+numero2 = int(dato2)
+restando = numero1 - numero2
+strResta = str(restando)
+resultado = "Su saldo disponible en la cuenta es " + strResta
+print(resultado)
+
+print("HOla")
