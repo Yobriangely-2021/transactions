87cf88256b0f8deb8aa9e3247b75cbcd3088ffc4
Yobriangely Mayora <yobriangely.mayora@gmail.com>
2021-05-20 22:37:57 -0300

    Otra vision del proyecto

--- International.py
+++ International.py
@@ -5,36 +5,37 @@ holidays = input()
 print("Usuario")
 nameone = input()
 
-print("Información de la cuenta")
+print("Cuenta")
 print("Venezolana Yobriangely Mayora: Cliente número:106")
 SALDO = "40000 $ "
 numbercuenta = "0021458952"
 print(f"Número de cuenta {numbercuenta}")
 print(f"Saldo disponible {SALDO}")
 
-print("Nombre y número de cuenta para transferir ")
+print("Propietario de la cuenta a transferir")
 Beatriz = input()
 
-print("Información de la cuenta")
+print("Cuenta")
 print("Venezolana Adriana Caraballo: Cliente número. 45")
 print("Número de cuenta: 60987545120")
 
 print("Informar el saldo de su tarjeta")
 dato1 = input()
+
 print(" Cantidad de dinero para transferir")
 dato2 = input()
-print("¿Acepta usted transferir este dinero?")
-yes = input()
+print("Lo sentimos, No es posible transferir esa cantidad de dinero, su saldo no  es suficiente. ")
 
-print("Dinero transferido correctamente")
+print("¿Desea depositar dinero? ")
+_yu = input
 
-print("¿Quiere saber el saldo de su cuenta?")
-horno = input()
+print("Cantidad a depositar")
+ju = input()
 
+print("Deposito realizado")
+print("saldo actual de su cuenta")
 
-numero1 = int(dato1)
-numero2 = int(dato2)
-restando = numero1 - numero2
-strResta = str(restando)
-resultado = "Su saldo disponible en la cuenta es " + strResta
-print(resultado)
+montoactual = 40000
+aderir = 40000
+duplicar = montoactual * aderir
+print(duplicar)
